104dbaa6cbf253f640448ed5f2642bb5d25bb793
Ahmed Fahed <Fahed@soft-park.de>
2017-03-21 17:39:50 +0100

    Add new Project

---
+++ Toleen ASP Project/ToleenProjekt/ToleenProjekt/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToleenProjekt.Controllers
+{
+    public class HomeController : Controller
+    {
+ 
+
+
+        public ActionResult Home()
+        {
+       
+            return View();
+        }
+        public ActionResult Go()
+        {
+
+            return View();
+        }
+        public ActionResult Come()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Toleen ASP Project/ToleenProjekt/ToleenProjekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToleenProjekt.Startup))]
+namespace ToleenProjekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
